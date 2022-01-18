3e2c9984e152c1cd004f16a021053a544791c171
Devin Pile <devinpile@gmail.com>
2022-01-18 09:25:32 -0700

    Add test for comp index

---
+++ spec/features/competition/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'competitions index' do
+  before do
+    @comp1 = Competition.create!(name: 'Regional Boys Baseball', location: 'Denver', sport: 'Baseball')
+    @comp2 = Competition.create!(name: 'Regional Girls Basketball', location: 'Boulder', sport: 'Basketball')
+    visit competitions_path
+  end
+
+  describe 'display' do
+    it 'names of all competitions' do
+      expect(page).to have_content(@comp1.name)
+      expect(page).to have_content(@comp2.name)
+    end
+  end 
+
